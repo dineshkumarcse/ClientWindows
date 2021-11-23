d5af4d8921d9443f7782d861b165bf748ae50170
dines <dinesh@jobgala.com>
2021-11-23 22:29:05 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResoponse = new System.Windows.Forms.Label();
+            this.btnRefesh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(182, 97);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(299, 97);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Stopwatch Server Time";
+            // 
+            // lblResoponse
+            // 
+            this.lblResoponse.AutoSize = true;
+            this.lblResoponse.Location = new System.Drawing.Point(358, 57);
+            this.lblResoponse.Name = "lblResoponse";
+            this.lblResoponse.Size = new System.Drawing.Size(71, 13);
+            this.lblResoponse.TabIndex = 4;
+            this.lblResoponse.Text = "lblResoponse";
+            // 
+            // btnRefesh
+            // 
+            this.btnRefesh.Location = new System.Drawing.Point(437, 97);
+            this.btnRefesh.Name = "btnRefesh";
+            this.btnRefesh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefesh.TabIndex = 5;
+            this.btnRefesh.Text = "Refresh";
+            this.btnRefesh.UseVisualStyleBackColor = true;
+            this.btnRefesh.Click += new System.EventHandler(this.btnRefesh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRefesh);
+            this.Controls.Add(this.lblResoponse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResoponse;
+        private System.Windows.Forms.Button btnRefesh;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        HubConnection connection = new HubConnectionBuilder()
+               .WithUrl("https://localhost:5001/Watch")
+               .Build();
+        public Form1()
+        {
+            InitializeComponent();
+            connection.StartAsync();
+            connection.InvokeCoreAsync("Get", args: new[] { "Dinesh" });
+            connection.On("Get", (string username) =>
+            {
+                lblResoponse.Text = username;
+            });
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            connection.StartAsync();
+            connection.InvokeCoreAsync("Start", args: new[] { "Dinesh" });
+            connection.On("Start", (string username) =>
+            {
+                lblResoponse.Text = username;
+            });
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            connection.StartAsync();
+            connection.InvokeCoreAsync("Stop", args: new[] { "Dinesh" });
+            connection.On("Stop", (string username) =>
+            {
+                lblResoponse.Text = username;
+            });
+
+        }
+        private void btnRefesh_Click(object sender, EventArgs e)
+        {
+            connection.StartAsync();
+            connection.InvokeCoreAsync("Get", args: new[] { "Dinesh" });
+            connection.On("Get", (string username) =>
+            {
+                lblResoponse.Text = username;
+            });
+        }
+    }
+}
